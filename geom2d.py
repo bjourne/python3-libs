5567bf76f4969f1c53f365e1c42a0cfc635407ba
Björn Lindqvist <bjourne@gmail.com>
2017-11-15 13:42:08 +0100

    not great code but it might work

--- geom2d.py
+++ geom2d.py
@@ -1,8 +1,26 @@
 # Copyright (C) 2017 Björn Lindqvist
+from bisect import bisect_left, insort
 from collections import namedtuple
 
 Point = namedtuple('Point', ['x', 'y'])
-LineSegment = namedtuple('LineSegment', ['p1', 'p2'])
+
+class LineSegment:
+    def __init__(self, p1, p2):
+        if p2.x < p1.x:
+            p1, p2 = p2, p1
+        elif p1.x == p2.x and p2.y < p1.y:
+            p1, p2 = p2, p1
+        self.p1 = p1
+        self.p2 = p2
+
+    def __eq__(self, o):
+        return self.p1 == o.p1 and self.p2 == o.p2
+
+    def __repr__(self):
+        return "LineSegment(%s,%s)" % (self.p1, self.p2)
+
+def line_segment(p1, p2):
+    return LineSegment(Point(*p1), Point(*p2))
 
 def orientation(seg, r):
     p = seg.p1
@@ -17,15 +35,14 @@ def colinear_intersect(seg, p):
         p.y <= max(seg.p1.y, seg.p2.y) and \
         p.y >= min(seg.p1.y, seg.p2.y)
 
-def line_segments_intersect(seg1, seg2):
-    '''Determine whether the open line segments seg1 and seg2
-    intersects.'''
-    (p1, p2), (p3, p4) = seg1, seg2
+def segments_intersect2(seg1, seg2):
+    '''Determine whether two open line segments intersect.'''
+    p1, p2, p3, p4 = seg1.p1, seg1.p2, seg2.p1, seg2.p2
+
     o1 = orientation(seg1, p3)
     o2 = orientation(seg1, p4)
     o3 = orientation(seg2, p1)
     o4 = orientation(seg2, p2)
-
     if o1 == 0:
         return colinear_intersect(seg1, p3)
     if o2 == 0:
@@ -35,3 +52,96 @@ def line_segments_intersect(seg1, seg2):
     if o4 == 0:
         return colinear_intersect(seg2, p2)
     return o1 != o2 and o3 != o4
+
+def segments_intersect_naive(segments):
+    '''Naive method to determine if a sequence of line segments has any
+    intersections.'''
+    for seg1 in segments:
+        for seg2 in segments:
+            if seg1 != seg2 and segments_intersect2(seg1, seg2):
+                return True
+    return False
+
+# Borrowed/stolen code from:
+class search:
+    def __init__(self):
+        self.values = []
+    def insert(self, value):
+        assert not value in self.values
+        insort(self.values, value)
+
+    def delete(self, value):
+        assert value in self.values
+        assert(self.values.pop(self.position(value)) == value)
+
+    def find_neighbors(self, value):
+        p = self.position(value)
+        l = None
+        r = None
+        if p > 0: l = self.values[p-1]
+        if p < len(self.values)-1: r = self.values[p+1]
+        return (l,r)
+
+    def position(self, value):
+        i = bisect_left(self.values, value)
+        if i != len(self.values) and self.values[i] == value:
+            return i
+        raise ValueError
+
+from itertools import groupby
+
+def segments_intersect_fast(segments):
+    def x1_key(seg):
+        return seg.p1.x
+    def y1_key(seg):
+        return seg.p1.y
+
+    # Special case for handling horizontal segments.
+    horiz = [seg for seg in segments if seg.p1.y == seg.p2.y]
+    for y, group in groupby(sorted(horiz, key = y1_key), key = y1_key):
+        group = sorted(group, key = x1_key)
+        scan = None
+        for seg in group:
+            if scan is not None and seg.p1.x < scan:
+                return True
+            scan = seg.p2.x
+
+    # Same kind of special casing for vertial segments.
+    vert = [seg for seg in segments if seg.p1.x == seg.p2.x]
+    for x, group in groupby(sorted(vert, key = x1_key), key = x1_key):
+        group = sorted(group, key = y1_key)
+        scan = None
+        for seg in group:
+            if scan is not None and seg.p1.y < scan:
+                return True
+            scan = seg.p2.y
+
+    # No handling for arbitrary colinear segments. They look way
+    # harder to handle.
+    end_points = []
+    for i, seg in enumerate(segments):
+        x1, y1, x2, y2 = seg.p1.x, seg.p1.y, seg.p2.x, seg.p2.y
+        is_right = x1 >= x2
+        end_points.append((x1, i, is_right))
+        end_points.append((x2, i, not is_right))
+    end_points = sorted(end_points)
+
+    tree = search()
+    for _, label, is_right in end_points:
+        seg = segments[label]
+        print(seg, is_right, tree.values)
+        if not is_right:
+            tree.insert(label)
+            for n in tree.find_neighbors(label):
+                if n is not None:
+                    if segments_intersect2(seg, segments[n]):
+                        return True
+        else:
+            p, s = tree.find_neighbors(label)
+            if p is not None and s is not None:
+                pred = segments[p]
+                succ = segments[s]
+                if segments_intersect2(pred, succ):
+                    return True
+            tree.delete(label)
+    return False
--- test_geom2d.py
+++ test_geom2d.py
@@ -1,6 +1,13 @@
 # Copyright (C) 2017 Björn Lindqvist
 from geom2d import LineSegment, Point, \
-    colinear_intersect, line_segments_intersect, orientation
+    colinear_intersect, line_segment, orientation, \
+    segments_intersect2, \
+    segments_intersect_fast, segments_intersect_naive
+
+def poly_to_segments(poly):
+    segments = list(zip(poly, poly[1:])) + [(poly[-1], poly[0])]
+    segments = [line_segment(*d) for d in segments]
+    return segments
 
 def test_line_segment():
     ls = LineSegment(Point(0, 3), Point(10, 0))
@@ -15,12 +22,15 @@ def test_orientation():
 def test_does_intersect():
     ls1 = LineSegment(Point(-10, 0), Point(10, 0))
     ls2 = LineSegment(Point(0, -10), Point(0, 10))
-    assert line_segments_intersect(ls1, ls2)
+    assert segments_intersect2(ls1, ls2)
     ls3 = LineSegment(Point(0, 0), Point(10, 0))
-    assert line_segments_intersect(ls1, ls3)
+    assert segments_intersect2(ls1, ls3)
     ls4 = LineSegment(Point(1, 6), Point(6, 1))
     ls5 = LineSegment(Point(2, 3), Point(9, 4))
-    assert line_segments_intersect(ls4, ls5)
+    assert segments_intersect2(ls4, ls5)
+    ls6 = LineSegment(Point(1, 1), Point(1, 4))
+    ls7 = LineSegment(Point(1, 4), Point(1, 3))
+    assert segments_intersect2(ls6, ls7)
 
 def test_colinear_intersect():
     ls = LineSegment(Point(0, 0), Point(10, 0))
@@ -29,10 +39,55 @@ def test_colinear_intersect():
     assert colinear_intersect(ls, p1)
     assert not colinear_intersect(ls, p2)
 
+def test_horiz_vert_lines():
+    ls1 = LineSegment(Point(0, 0), Point(10, 0))
+    ls2 = LineSegment(Point(5, 0), Point(15, 0))
+    assert segments_intersect2(ls1, ls2)
+    ls1 = LineSegment(Point(0, 0), Point(0, 10))
+    ls2 = LineSegment(Point(0, 5), Point(0, 15))
+    assert segments_intersect2(ls1, ls2)
+
 def test_does_not_intersect():
     ls1 = LineSegment(Point(-10, 0), Point(10, 0))
     ls2 = LineSegment(Point(10, 0), Point(20, 0))
-    assert not line_segments_intersect(ls1, ls2)
+    assert not segments_intersect2(ls1, ls2)
     ls3 = LineSegment(Point(1, 6), Point(5, 7))
     ls4 = LineSegment(Point(5, 7), Point(9, 4))
-    assert not line_segments_intersect(ls3, ls4)
+    assert not segments_intersect2(ls3, ls4)
+
+def test_segments_intersect():
+    poly = [(1, 6), (5, 7), (9, 4), (2, 3), (6, 1)]
+    segments = poly_to_segments(poly)
+    assert segments_intersect_naive(segments)
+    assert segments_intersect_fast(segments)
+
+def test_segments_no_intersect():
+    poly = [(1, 6), (5, 7), (9, 4), (4, 3), (7, 4), (4, 6), (3, 1)]
+    segments = poly_to_segments(poly)
+    assert not segments_intersect_naive(segments)
+    assert not segments_intersect_fast(segments)
+
+def test_segments_horrible_intersect():
+    poly = [(1, 1), (1, 4), (1, 3), (2, 2), (3, 1), (3, 2), (2, 2)]
+    segments = poly_to_segments(poly)
+    print(segments)
+    assert segments_intersect_naive(segments)
+    assert segments_intersect_fast(segments)
+
+def test_segments_horizontal_overlap():
+    ls1 = LineSegment(Point(0, 0), Point(10, 0))
+    ls2 = LineSegment(Point(5, 0), Point(15, 0))
+    segments = [ls1, ls2]
+    assert segments_intersect_fast(segments)
+
+def test_segments_vertical_overlap():
+    ls1 = LineSegment(Point(0, 0), Point(0, 10))
+    ls2 = LineSegment(Point(0, 5), Point(0, 15))
+    segments = [ls1, ls2]
+    assert segments_intersect_naive(segments)
+    assert segments_intersect_fast(segments)
+    ls1 = LineSegment(Point(1, 1), Point(1, 4))
+    ls2 = LineSegment(Point(1, 4), Point(1, 3))
+    segments = [ls1, ls2]
+    assert segments_intersect_naive(segments)
+    assert segments_intersect_fast(segments)
