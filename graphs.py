48300c13ec7365450e8e0ad779ed2a3b1a2b36a6
Björn Lindqvist <bjourne@gmail.com>
2018-02-02 18:37:26 +0100

    pretty good

--- graphs.py
+++ graphs.py
@@ -2,25 +2,24 @@ from collections import deque
 from itertools import islice, product
 
 def bfs(graph, start, accepting):
-    queue = deque([[(None, start)]])
-    seen = set()
+    queue = deque([[(start, None)]])
     while queue:
         path = queue.popleft()
-        node = path[-1][1]
+        node = path[-1][0]
         if node in accepting:
-            tpath = tuple(path)
-            if len(tpath) > 1 and tpath not in seen:
-                seen.add(tpath)
-                yield tpath
-        adjacents = graph.get(node, [])
-        for trans in adjacents:
+            yield path
+        adjacents = graph.get(node, {})
+        for item in adjacents.items():
             new_path = list(path)
-            new_path.append(trans)
+            new_path.append(item)
             queue.append(new_path)
 
 def add_transitions(trans, chars, to):
-    gen = range(ord(chars[0]), ord(chars[-1]) + 1)
-    trans.append((gen, to))
+    if to not in trans:
+        trans[to] = ""
+    char_range = range(ord(chars[0]), ord(chars[-1]) + 1)
+    extra_chars = ''.join(chr(o) for o in char_range)
+    trans[to] = trans[to] + extra_chars
 
 def read_ints_line():
     return [int(x) for x in input().split()]
@@ -34,9 +33,8 @@ def read_dfa():
         from_, to, chars = input().split()
         from_ = int(from_)
         to = int(to)
-
         if from_ not in graph:
-            graph[from_] = []
+            graph[from_] = {}
         trans = graph[from_]
         add_transitions(trans, chars, to)
     path_count = read_ints_line()[0]
@@ -44,20 +42,23 @@ def read_dfa():
 
 def generate_paths(path_gen):
     for path_pattern in path_gen:
-        gens = [g[0] for g in path_pattern[1:]]
-        for path in product(*gens):
-            yield ''.join(chr(o) for o in path)
+        # print(path_pattern)
+        gens = [g[1] for g in path_pattern[1:]]
+        if gens:
+            for path in product(*gens):
+                yield ''.join(path)
 
 def main():
     graph, start, accepting, path_count = read_dfa()
     gen = bfs(graph, start, accepting)
     paths = islice(generate_paths(gen), path_count)
     paths = list(paths)
+
     print(len(paths))
     for path in paths:
         print(path)
 
 if __name__ == '__main__':
-    # main()
-    import cProfile
-    cProfile.run('main()')
+    main()
+    # import cProfile
+    # cProfile.run('main()')
