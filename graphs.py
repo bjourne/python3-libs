327df2d33db52bbd4e336e239cb60a5172d0d3f2
Björn Lindqvist <bjourne@gmail.com>
2018-02-03 19:50:26 +0100

    faster with a list instead of a dict i hope

--- graphs.py
+++ graphs.py
@@ -1,5 +1,9 @@
+# 28/36
+# 29/36 med len(path)
+
 from collections import deque
 from itertools import product
+from os import write
 
 def travel(G, start, dests, count):
     queue = deque([[(start, None)]])
@@ -30,41 +34,27 @@ def read_ints_line():
     return [int(x) for x in input().split()]
 
 def read_dfa():
-    G = {0 : {}}
-    _, start = read_ints_line()
+    state_count, start = read_ints_line()
     accepting = set(read_ints_line()[1:])
     trans_count = read_ints_line()[0]
+    G = [{} for x in range(state_count)]
     for x in range(trans_count):
         from_, to, chars = input().split()
         from_ = int(from_)
         to = int(to)
-        if from_ not in G:
-            G[from_] = {}
-        if to not in G:
-            G[to] = {}
         trans = G[from_]
         add_transitions(trans, chars, to)
     path_count = read_ints_line()[0]
     return G, start, accepting, path_count
 
-def perf_test():
-    G = {
-        1 : {2 : 'mqr', 3 : 't'},
-        2 : {3 : 'q', 2 : 'q'},
-        3 : {1 : 'S', 4 : '_'},
-        4 : {3 : 't', 1 : 'xUI', 5 : '=+/'},
-        5 : {4 : 'q', 6 : '1'},
-        6 : {1 : '0'}
-    }
-    for p in travel(G, 1, [6], 1000000):
-        print(p)
-
 def main():
     G, start, accepting, path_count = read_dfa()
     paths = travel(G, start, accepting, path_count)
-    print(len(paths))
-    for path in paths:
-        print(path)
+    lines = [str(len(paths))]
+    lines.extend(paths)
+    lines.append('')
+    s = '\n'.join(lines)
+    write(1, s.encode('ascii'))
 
 if __name__ == '__main__':
     main()
