6b0a6d8fbf3f72f8bdcdc0c92f4ee3dfe8f7b485
Björn Lindqvist <bjourne@gmail.com>
2018-02-03 17:14:37 +0100

    generators removed

--- graphs.py
+++ graphs.py
@@ -1,22 +1,23 @@
 from collections import deque
-from itertools import islice, product
+from itertools import product
 
-def generate_paths(path_gen):
-    for path_pattern in path_gen:
-        gens = [g[1] for g in path_pattern[1:]]
-        for path in product(*gens):
-            yield ''.join(path)
-
-def bfs(graph, start, accepting):
+def travel(G, start, dests, count):
     queue = deque([[(start, None)]])
+    paths = []
     while queue:
         path = queue.popleft()
-        node = path[-1][0]
-        if node in accepting:
-            yield path
-        adjacents = graph.get(node, {})
-        for item in adjacents.items():
+        at = path[-1][0]
+        if at in dests:
+            pp = [p[1] for p in path[1:]]
+            for real_path in product(*pp):
+                paths.append(''.join(real_path))
+                count -= 1
+                if count == 0:
+                    return paths
+        adj = G[at]
+        for item in adj.items():
             queue.append(path + [item])
+    return paths
 
 def add_transitions(trans, chars, to):
     if to not in trans:
@@ -29,31 +30,41 @@ def read_ints_line():
     return [int(x) for x in input().split()]
 
 def read_dfa():
-    graph = {}
+    G = {0 : {}}
     _, start = read_ints_line()
-    accepting = tuple(read_ints_line()[1:])
+    accepting = set(read_ints_line()[1:])
     trans_count = read_ints_line()[0]
     for x in range(trans_count):
         from_, to, chars = input().split()
         from_ = int(from_)
         to = int(to)
-        if from_ not in graph:
-            graph[from_] = {}
-        trans = graph[from_]
+        if from_ not in G:
+            G[from_] = {}
+        if to not in G:
+            G[to] = {}
+        trans = G[from_]
         add_transitions(trans, chars, to)
     path_count = read_ints_line()[0]
-    return graph, start, accepting, path_count
+    return G, start, accepting, path_count
+
+def perf_test():
+    G = {
+        1 : {2 : 'mqr', 3 : 't'},
+        2 : {3 : 'q', 2 : 'q'},
+        3 : {1 : 'S', 4 : '_'},
+        4 : {3 : 't', 1 : 'xUI', 5 : '=+/'},
+        5 : {4 : 'q', 6 : '1'},
+        6 : {1 : '0'}
+    }
+    for p in travel(G, 1, [6], 1000000):
+        print(p)
 
 def main():
-    graph, start, accepting, path_count = read_dfa()
-    gen = bfs(graph, start, accepting)
-    paths = islice(generate_paths(gen), path_count)
-    paths = list(paths)
+    G, start, accepting, path_count = read_dfa()
+    paths = travel(G, start, accepting, path_count)
     print(len(paths))
     for path in paths:
         print(path)
 
 if __name__ == '__main__':
     main()
-    # import cProfile
-    # cProfile.run('main()')
