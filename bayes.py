35dcd43d402bc407c895ae419c85ebbbe1122753
Björn Lindqvist <bjourne@gmail.com>
2018-03-06 16:20:39 +0100

    boost classifyer works

--- bayes.py
+++ bayes.py
@@ -7,11 +7,17 @@
 #   Y: list of classes for a list of samples
 #   W: list of weights for each sample
 #   K: list of unique classes
+from math import exp
 from numpy import (argmax, array,
-                   cov, diag, full,
+                   cov,
+                   diag, dot,
+                   full,
+                   histogram,
                    identity,
                    log,
-                   unique)
+                   shape,
+                   unique,
+                   zeros)
 from numpy.linalg import det, inv
 
 def make_cov(X, W):
@@ -47,12 +53,13 @@ def discr(x, prior, mu, sigma):
         + log(prior)
 
 def classify_bayes(X, prior, mu, sigma):
-    hs = []
     for x in X:
         vals = [discr(x, p, m, s) for (p, m, s) in zip(prior, mu, sigma)]
-        h = argmax(vals, axis = 0)
-        hs.append(h)
-    return hs
+        yield argmax(vals, axis = 0)
+
+def init_weights(X):
+    N = len(X)
+    return full((N,), 1/N)
 
 class BayesClassifier:
     def __init__(self):
@@ -63,9 +70,54 @@ class BayesClassifier:
     def fit(self, X, Y, W = None):
         N = len(X)
         if W is None:
-            W = full((N,), 1/N)
+            W = init_weights(X)
         self.prior = compute_prior(Y, W)
         self.mu, self.sigma = ml_params(X, Y, W)
 
     def predict(self, X):
         return classify_bayes(X, self.prior, self.mu, self.sigma)
+
+def train_boost(clf, X, Y, N):
+    # Initialize all weights uniformly
+    W = init_weights(X)
+    insts = []
+    alphas = []
+    for _ in range(N):
+        # Train weak learner
+        inst = clf()
+        inst.fit(X, Y, W)
+        # Get weak hypothesis
+        ht = inst.predict(X)
+
+        # Compute its error, et.
+        deltas = [0 if v == l else 1 for v, l in zip(ht, Y)]
+        et = dot(W, deltas)
+        alpha = 0.5 * (log(1 - et) - log(et))
+
+        # Update W
+        growth = [exp(-alpha) if d == 0 else exp(alpha) for d in deltas]
+        W = W*growth
+        # Normalize
+        W = W / float(W.sum())
+        alphas.append(alpha)
+        insts.append(inst)
+    return insts, alphas
+
+class BoostClassifier:
+    def __init__(self, base_clf, count):
+        self.base_clf = base_clf
+        self.count = count
+
+    def fit(self, X, Y):
+        self.n_classes = len(unique(Y))
+        self.insts, self.alphas = train_boost(self.base_clf, X, Y,
+                                              self.count)
+
+    def predict(self, X):
+        # One hypothesis for each classifier.
+        hypothesis = [inst.predict(X) for inst in self.insts]
+        for votes in zip(*hypothesis):
+            tally = [0] * self.n_classes
+            for alpha, vote in zip(self.alphas, votes):
+                tally[vote] += alpha
+            yield argmax(tally, axis = 0)
--- test_bayes.py
+++ test_bayes.py
@@ -1,4 +1,4 @@
-from bayes import BayesClassifier
+from bayes import BayesClassifier, BoostClassifier
 from numpy import allclose, array, array_equal
 from sklearn.datasets import load_iris
 
@@ -18,9 +18,25 @@ def test_naive_bayes():
 def test_classifying():
     bc = BayesClassifier()
     bc.fit(irisX, irisY)
-    irisPredY = bc.predict(irisX)
+    irisPredY = list(bc.predict(irisX))
     # Only six mismatches
     assert sum(irisY == irisPredY) == 144
+
+def test_boosting():
+    bc = BoostClassifier(BayesClassifier, 10)
+    bc.fit(irisX, irisY)
+    expected = [1.589, 1.022, 0.591, 1.398, 0.637,
+                0.276, -0.242, 0.081, -0.072, 0.002]
+    assert allclose(bc.alphas, expected, atol = 10e-3)
+    irisPredY = list(bc.predict(irisX))
+    assert sum(irisY == irisPredY) == 147
+
+def test_boosting_one():
+    bc = BoostClassifier(BayesClassifier, 1)
+    bc.fit(irisX, irisY)
+    irisPredY = list(bc.predict(irisX))
+    assert sum(irisY == irisPredY) == 144
+
 
 if __name__ == '__main__':
     bc = BayesClassifier()
