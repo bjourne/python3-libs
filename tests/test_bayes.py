fc615b1709e27105b3fedcda6342771c51bb3261
Björn Lindqvist <bjourne@gmail.com>
2019-04-09 17:07:17 +0200

    fixing the values... maybe the dataset has changed?

--- tests/test_bayes.py
+++ tests/test_bayes.py
@@ -11,7 +11,7 @@ def test_naive_bayes():
     assert bc.prior is None
     bc.fit(irisX, irisY)
     assert array_equal(bc.prior, [1/3, 1/3, 1/3])
-    assert allclose(bc.mu, [[5.006, 3.418, 1.464, 0.244],
+    assert allclose(bc.mu, [[5.006, 3.428, 1.462, 0.246],
                             [5.936, 2.77,  4.26,  1.326],
                             [6.588, 2.974, 5.552, 2.026]])
 
