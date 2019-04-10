362b9ff237b0223ed373588967a35096a1bdcaca
Björn Lindqvist <bjourne@gmail.com>
2019-04-10 04:36:56 +0200

    tests in its own file

--- lambdac.py
+++ lambdac.py
@@ -138,66 +138,6 @@ def step(ast):
         else:
             return Appl(step(lhs), rhs)
 
-# See https://www.easycalculation.com/analytical/lambda-calculus.php
-def test_format():
-    assert format(parse(r'a (\b. a) c')) == r'a (\b. a) c'
-    assert format(parse(r'(\b. a) c')) == r'(\b. a) c'
-    assert format(parse('(a b) (c d)')) == 'a b (c d)'
-    assert format(parse('a (b c)')) == 'a (b c)'
-    str3 = r'\x. \y. z \m. o'
-    assert format(parse(str3)) == r'\x y. z \m. o'
-    assert format(parse(str3), brackets = True) \
-        == r'(\x y. (z (\m. o)))'
-
-    s = 'x y z'
-    assert format(parse(s), brackets = True) == '((x y) z)'
-    assert format(parse(s)) == 'x y z'
-
-    examples = [
-        (r'(\b. \c. b c (\t. \f. f)) (\t. \f. f)',
-         r'(\b c. b c \t f. f) \t f. f'),
-        (r'(\n. \s. \z. s (n s z)) (\s. \z. s z)',
-         r'(\n s z. s (n s z)) \s z. s z'),
-        (r'(\x. x) (\y. y)',
-         r'(\x. x) \y. y'),
-        (r'((a (\x. x)) a)', r'a (\x. x) a')
-        ]
-    for inp, out in examples:
-        assert format(parse(inp)) == out
-
-def test_parsing():
-    # Support for parsing abstraction contraction
-    expr = parse(r'\x y z. x')
-    assert expr.id == 'x'
-    assert expr.body.id == 'y'
-    assert expr.body.body.id == 'z'
-    assert expr.body.body.body.id == 'x'
-
-    examples = [
-        (r'(\n. \s. \z. s (n s z)) (\s. \z. z)',
-         r'(\n s z. s (n s z)) \s z. z'),
-        (r'(\n s z. s (n s z)) (\s z. z)',
-         r'(\n s z. s (n s z)) \s z. z'),
-        ]
-    for inp, out in examples:
-        assert format(parse(inp)) == out
-
-
-def test_step():
-    examples = [
-        (r'(\x. x) y', 'y'),
-        (r'(\x. z) y', 'z'),
-        (r'(\x. x x) y', 'y y'),
-        (r'(\x. (\y. k)) k', '\y. k')
-        ]
-    for inp, out in examples:
-        expr = parse(inp)
-        expr = step(expr)
-        assert format(expr) == out
-
 if __name__ == '__main__':
-    test_format()
-    test_step()
-    test_parsing()
     expr = parse(r'(\f y. f y) (\x. y) q')
     print(format(step(step(step(expr)))))
