c7826d1cb4a44093e50720ccd991d62e53287a7d
Björn Lindqvist <bjourne@gmail.com>
2019-04-22 01:55:12 +0200

    golfing 1747

--- lambdac_golf.py
+++ lambdac_golf.py
@@ -1,17 +1,9 @@
-from collections import *
-import re
+from collections import *;import re
 A,B,y,c=namedtuple('A',['l','r']),namedtuple('B',['i','b']),type,list.pop
-def ab(t):
-    c(t,0);p = c(t,0);c(t,0)
-    return B(p,tm(t))
-def tm(t):
-    return ab(t)if t[0] == '\\'else ap(t)
+def ab(t):c(t,0);p = c(t,0);c(t,0);return B(p,tm(t))
+def tm(t):return ab(t)if t[0]=='\\'else ap(t)
 def at(t):
-    if t[0] == '(':
-        c(t,0)
-        trm = tm(t)
-        c(t,0)
-        return trm
+    if t[0] == '(':c(t,0);trm=tm(t);c(t,0);return trm
     if ord('a')<=ord(t[0][0])<=ord('z'):return c(t,0)
     if t[0]=='\\':return ab(t)
 def ap(t):
@@ -20,8 +12,7 @@ def ap(t):
         r = at(t)
         if not r:return l
         l = A(l, r)
-def P(s):
-    return tm(re.findall(r'(\(|\)|\\|[a-z]\w*|\.)',s)+['='])
+def P(s):return tm(re.findall(r'(\(|\)|\\|[a-z]\w*|\.)',s)+['='])
 def V(e):o=y(e);return V(e.b)-{e.i} if o==B else V(e.l)|V(e.r)if o==A else{e}
 def R(e,f,t):return B(e.i,R(e.b,f,t)) if y(e)==B else A(R(e.l,f,t),R(e.r,f,t))if y(e)==A else t if e==f else e
 def N(i,e):return N(chr(97+(ord(i[0])-96)%26),e) if i in V(e)else i
@@ -35,18 +26,12 @@ def T(e):
     if o==A:
         l,r=e
         return S(l.i,l.b,r)if y(l)==B else A(T(l),r)if y(l)==A else o(l,T(r))
-    if o==B:
-        return o(e.i, T(e.b))
+    if o==B:return o(e.i, T(e.b))
     raise RuntimeError('hi')
 def E(a):
     try: return E(T(a))
-    except RuntimeError:
-        return a
-def F(e):
-    o=y(e)
-    if o==B:return r'(\%s. %s)'%(e.i,F(e.b))
-    if o==A:return r'(%s %s)'%(F(e.l),F(e.r))
-    return e
+    except RuntimeError:return a
+def F(e):o=y(e);return r'(\%s. %s)'%(e.i,F(e.b)) if o==B else '(%s %s)'%(F(e.l),F(e.r)) if o==A else e
 if __name__ == '__main__':
     expr = P(r'((\a. (\b. (a (a (a b))))) (\ c. (\ d. (c (c d)))))')
     print(F(E(expr)))
