082b60f426be9c64e58d7258d877a6819b2fc91b
Björn Lindqvist <bjourne@gmail.com>
2021-03-19 08:06:28 +0100

    more tests

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -54,3 +54,31 @@ def test_slide_window_without_padding():
                                     [4, 5],
                                     [6, 7],
                                     [8, 9]])
+
+def test_small_array():
+    windows = slide_window(np.arange(5), 10, 10, None)
+    assert windows.shape == (0, 10)
+    windows = slide_window(np.arange(5), 10, 10, -1)
+    assert windows.shape == (1, 10)
+
+def test_big_stuff():
+    vs = 100
+    A = np.random.randint(vs, size = 100_000)
+    delim = np.random.randint(vs)
+    I = np.nonzero(A == delim)[0]
+    P = np.split(A, I)
+
+    print(P[0].flags['OWNDATA'])
+
+    P = [P[0]] + [p[1:] for p in P[1:]]
+    P = [np.array(p) for p in P if len(p) > 0]
+    assert len(I) + sum(len(p) for p in P) == len(A)
+
+    for p in P:
+        assert not delim in p
+
+    P = [slide_window(p, 50, 49, None) for p in P]
+    for p in P:
+        for w in p:
+            #print(delim, w)
+            assert not delim in w
