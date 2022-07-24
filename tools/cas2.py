144f1032d90eae1ca9371cf01c148469607cb12f
Björn Lindqvist <bjourne@gmail.com>
2022-07-24 15:13:28 +0200

    solving many corner cases

--- tools/cas2.py
+++ tools/cas2.py
@@ -14,6 +14,11 @@ def call(n, args):
 def const(v):
     return Constant(v)
 
+CONST_0 = const(0)
+CONST_1 = const(1)
+CONST_N1 = const(-1)
+CONST_HALF = const(Fraction(1, 2))
+
 def commut_join(id, args):
     flat = []
     for a in args:
@@ -23,48 +28,58 @@ def commut_join(id, args):
             flat.append(a)
     return call(id, flat)
 
-def mul(args):
+def mul(*args):
+    assert len(args) > 1
     return commut_join('mul', args)
 
-def add(args):
+def add(*args):
     return commut_join('add', args)
 
-def pow(l, r):
-    if type(l) == Call:
-        id = l.func.id
-        if id == 'pow':
-            # Not sure about this (probably wrong).
-            return pow(l.args[0], mul([l.args[1], r]))
-    return call('pow', [l, r])
+def sub(l, r):
+    return add(l, mul(CONST_N1, r))
+
+def pow(*args):
+    assert len(args) == 2
+    return call('pow', args)
 
 def div(l, r):
-    return mul([l, pow(r, const(-1))])
+    return mul(l, pow(r, CONST_N1))
+
+BINOPS = {
+    Add: add,
+    Sub: sub,
+    Mult: mul,
+    Pow: pow,
+    Div: div
+}
 
 def build(tree):
     tp = type(tree)
     if tp == BinOp:
         tp_op = type(tree.op)
-        l = build(tree.left)
-        r = build(tree.right)
-        if tp_op == Add:
-            return add([l, r])
-        elif tp_op == Sub:
-            return add([l, mul([const(-1), r])])
-        elif tp_op == Mult:
-            return mul([l, r])
-        elif tp_op == Pow:
-            return pow(l, r)
-        elif tp_op == Div:
-            return div(l, r)
-        assert False
+        args = [build(tree.left), build(tree.right)]
+        return BINOPS[tp_op](*args)
     elif tp == Call:
         id = tree.func.id
         if id == 'sqrt':
-            return pow(tree.args[0], const(Fraction(1, 2)))
+            return pow(tree.args[0], CONST_HALF)
         return call(id, [build(a) for a in tree.args])
     elif tp == UnaryOp:
-        return mul([const(-1), build(tree.operand)])
+        return mul(CONST_N1, build(tree.operand))
     return tree
+
+def fold_add_key_value(arg):
+    tp = type(arg)
+    if tp == Name:
+        return arg, 1
+    if tp == Call and arg.func.id == 'mul':
+        head, tail = arg.args[0], arg.args[1:]
+        if type(head) == Constant:
+            if len(tail) == 1:
+                return tail[0], head.value
+            return mul(*tail), head.value
+        return arg, 1
+    assert False
 
 def fold_add(args):
     coeff = 0
@@ -74,36 +89,26 @@ def fold_add(args):
         if tp == Constant:
             coeff += arg.value
         else:
-            if tp == Name:
-                key = arg
-                value = 1
-            elif tp == Call and arg.func.id == 'mul':
-                mul_args = arg.args
-                head, tail = mul_args[0], mul_args[1:]
-                if type(head) == Constant:
-                    key = mul(tail)
-                    value = head.value
-                else:
-                    key = mul(mul_args)
-                    value = 1
-            else:
-                assert False
+            key, value = fold_add_key_value(arg)
             key = unparse(key)
             counts[key] += value
+
     counts = sorted(counts.items())
+
     head = const(coeff)
-    tail = [mul([const(v), parse_expr(k)]) for (k, v) in counts]
-    tail = [fold(e) for e in tail]
+    tail = [fold(mul(const(v), parse_expr(k))) for (k, v) in counts
+            if v]
     if not tail:
         return head
     if coeff == 0:
         if len(tail) == 1:
             return tail[0]
-        return add(tail)
-    return add([head] + tail)
+        return add(*tail)
+    args = [head] + tail
+    return add(*args)
 
 def fold_mul(args):
-    counts = defaultdict(lambda: add([]))
+    counts = defaultdict(lambda: add())
     coeff = 1
     for arg in args:
         tp = type(arg)
@@ -124,21 +129,21 @@ def fold_mul(args):
             else:
                 assert False
             key = unparse(key)
-            counts[key].args.append(value)
+            counts[key] = add(counts[key], value)
 
     counts = sorted(counts.items())
-    head = const(coeff)
-    tail = [pow(parse_expr(k), v) for (k, v) in counts]
-    tail = [fold(e) for e in tail]
-    if not tail:
-        return head
-    elif coeff == 1:
-        if len(tail) == 1:
-            return tail[0]
-        return mul(tail)
-    elif coeff == 0:
-        return const(0)
-    return mul([head] + tail)
+    args = [fold(pow(parse_expr(k), v)) for k, v in counts]
+    args = [const(coeff)] + args
+    if coeff == 0:
+        return CONST_0
+    if coeff == 1:
+        args = args[1:]
+    n_args = len(args)
+    if n_args == 0:
+        return CONST_1
+    elif n_args == 1:
+        return args[0]
+    return mul(*args)
 
 def fold_pow(args):
     base, power = args
@@ -162,35 +167,66 @@ def fold_pow(args):
     if tp_base == Constant:
         if base.value == 1:
             return const(1)
-    elif tp_base == Call and base.func.id == 'mul':
-        tree = mul([pow(a, power) for a in base.args])
-        return fold(tree)
-    return pow(base, power)
+    elif tp_base == Call:
+        id = base.func.id
+        if id == 'mul':
+            tree = mul(*[pow(a, power) for a in base.args])
+            return fold(tree)
+        if id == 'pow':
+            tree = pow(base.args[0], mul(base.args[1], power))
+            return fold(tree)
+    return pow(*args)
 
-def fold_usub(tree):
-    tp = type(tree)
-    return Constant(-tree.value)
+FOLDERS = {
+    'add' : fold_add,
+    'mul' : fold_mul,
+    'pow' : fold_pow
+}
 
 def fold(tree):
-    FOLDERS = {
-        'add' : fold_add,
-        'mul' : fold_mul,
-        'pow' : fold_pow
-    }
     tp = type(tree)
     if tp == Call:
         id = tree.func.id
         folder = FOLDERS.get(id, lambda x: call(id, x))
-        return folder([fold(a) for a in tree.args])
+        tree2 = folder([fold(a) for a in tree.args])
     elif tp == UnaryOp:
-        return fold_usub(tree.operand)
-    return tree
+        tree2 = Constant(-tree.operand.value)
+    else:
+        tree2 = tree
+    #print('%s => %s' % (unparse(tree), unparse(tree2)))
+    return tree2
 
 # All this shit mostly to get negative numbers to look ok.
 def is_negative(tree):
     if type(tree) == Call and tree.func.id == 'mul':
         return tree.args[0].value < 0
     return False
+
+def tidy_pow(b, e):
+    b = tidy(b, False)
+    if type(b) == Constant and b.value < 0:
+        b = UnaryOp(USub(), const(-b.value))
+    if type(e) == Constant:
+        e_val = e.value
+        if e_val == Fraction(1, 2):
+            return call('sqrt', [b])
+        if e_val < 0:
+            e = UnaryOp(USub(), const(-e_val))
+    e = tidy(e, False)
+    return BinOp(b, Pow(), e)
+
+def tidy_mul(args, negate):
+    ret = tidy(args.pop(0), False)
+    if type(ret) == Constant and ret.value < 0:
+        if negate:
+            ret.value = -ret.value
+            if ret.value == 1:
+                ret = tidy(args.pop(0), False)
+        elif ret.value == -1:
+            ret = UnaryOp(USub(), args.pop(0))
+    for arg in args:
+        ret = BinOp(ret, Mult(), tidy(arg, False))
+    return ret
 
 def tidy(tree, negate):
     tp = type(tree)
@@ -202,33 +238,18 @@ def tidy(tree, negate):
             ret = BinOp(ret, op, tidy(arg, negate))
         return ret
     elif tp == Call and tree.func.id == 'mul':
-        args = tree.args
-        ret = tidy(args.pop(0), False)
-        if type(ret) == Constant and ret.value < 0:
-            if negate:
-                ret.value = -ret.value
-                if ret.value == 1:
-                    ret = tidy(args.pop(0), False)
-            elif ret.value == -1:
-                ret = UnaryOp(USub(), args.pop(0))
-        for arg in args:
-            ret = BinOp(ret, Mult(), tidy(arg, False))
-        return ret
+        return tidy_mul(tree.args, negate)
     elif tp == Call and tree.func.id == 'pow':
-        base = tidy(tree.args[0], False)
-        if type(base) == Constant and base.value < 0:
-            base = UnaryOp(USub(), const(-base.value))
-        power = tree.args[1]
-        if type(power) == Constant and power.value == Fraction(1, 2):
-            return call('sqrt', [base])
-        power = tidy(power, False)
-        return BinOp(base, Pow(), power)
+        return tidy_pow(*tree.args)
+    elif tp == Call:
+        return call(tree.func.id, [tidy(a, False) for a in tree.args])
     elif tp == Constant:
         v = tree.value
         if type(v) == Fraction:
-            return BinOp(const(v.numerator),
-                         Div(),
-                         const(v.denominator))
+            p, q = v.numerator, v.denominator
+            if q == 1:
+                return const(p)
+            return BinOp(const(p), Div(), const(q))
     return tree
 
 def prettify(expr):
@@ -242,6 +263,11 @@ def check(output, expected):
 
 def test_build():
     exprs = [
+
+        # Negative exponents
+        ('x**(-z)*x**(z + 1)', 'mul(pow(x, mul(-1, z)), pow(x, add(z, 1)))'),
+
+        ('x*y', 'mul(x, y)'),
         ('(-x)*(-(y*-z))', 'mul(-1, x, -1, y, -1, z)'),
         ('x+x*y+z', 'add(x, mul(x, y), z)'),
         ('x*(a+b+c)', 'mul(x, add(a, b, c))'),
@@ -251,7 +277,6 @@ def test_build():
         ('3*x - 10*x', 'add(mul(3, x), mul(-1, 10, x))'),
         ('x**(1/2)*x**2', 'mul(pow(x, mul(1, pow(2, -1))), pow(x, 2))'),
         ('4**(1/2)', 'pow(4, mul(1, pow(2, -1)))'),
-        ('(x**y)**z', 'pow(x, mul(y, z))'),
         ('3**(x+y)+3**(y+x)', 'add(pow(3, add(x, y)), pow(3, add(y, x)))'),
         ('cos(x+y)+cos(y+x)', 'add(cos(add(x, y)), cos(add(y, x)))'),
         ('cos(-5*(x+y))', 'cos(mul(-1, 5, add(x, y)))'),
@@ -288,7 +313,10 @@ def test_fold():
         ('sqrt(x)**2', 'x'),
         ('sqrt(x)*sqrt(x)', 'x'),
 
-        # Integer powers
+        # Trigonometry
+        ('cos(-x)', 'cos(mul(-1, x))'),
+
+        # Integer exponents
         ('x**1*x**(-1)', '1'),
         ('x**1', 'x'),
         ('(a + b + c)**0', '1'),
@@ -296,6 +324,7 @@ def test_fold():
         ('-3*x*8*x', 'mul(-24, pow(x, 2))'),
         ('3*x**2 - 10*x**2 + 8', 'add(8, mul(-7, pow(x, 2)))'),
         ('x**2*x', 'pow(x, 3)'),
+        ('x**(-z)*x**(z + 1)', 'x'),
 
         # Imaginary numbers
         ('(-1)**(1/2)', 'I'),
@@ -308,7 +337,7 @@ def test_fold():
         ('(x+4)**n*(x+4)', 'pow(add(4, x), add(1, n))'),
         ('(x+y)*(y+x)', 'pow(add(x, y), 2)'),
         ('(x+y+z)*(y+x+z)*(z+x+y)', 'pow(add(x, y, z), 3)'),
-        ('cos(x-y)*cos(x-y)', 'pow(cos(add(mul(-1, y), x)), 2)')
+        ('cos(x-y)*cos(x-y)', 'pow(cos(add(x, mul(-1, y))), 2)')
     ]
     for input, expected in exprs:
         tree = parse_expr(input)
@@ -318,14 +347,28 @@ def test_fold():
 
 def test_tidy():
     exprs = [
+        # Constant expressions
         ('2**(1/2)', 'sqrt(2)'),
+        ('(2/10)*10', '2'),
+
+        # Linear multivar
+        ('(-x)*(-(y*-z*3))', '-3xyz'),
+
+        # Negative exponents
+        ('x**(-z)*y**(-1)', 'x^(-z)y^(-1)'),
+        ('x**(-z)*x**(z + 1)', 'x'),
+
+        # Trig
+        ('cos(-x)**(-y)', 'cos(-x)^(-y)'),
+
         ('-1*x', '-x'),
         ('-(-(-(x)))', '-x'),
         ('8 - w*75*x*y', '8 - 75wxy'),
         ('3*x**2 - 10*x**2 + 8', '8 - 7x^2'),
         ('(-5)**(1/8)', '(-5)^(1 / 8)'),
         ('4**(1/2)', 'sqrt(4)'),
-        ('(x**y)**z', 'x^(yz)')
+        ('(x**y)**z', 'x^(yz)'),
+
     ]
     for input, expected in exprs:
         tree = parse_expr(input)
